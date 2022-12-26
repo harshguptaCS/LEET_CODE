4fd3f6d48e4a39c5952d792f2256f2902a83649d
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2022-12-26 20:56:55 +0530

    Create LC_add_digits.java

---
+++ LC_add_digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+       while(num>9){
+           int x=0;
+           while(num!=0){
+               x+=num%10;
+               num=num/10;
+           }
+           num=x;
+       } 
+       return num;
+    }
+}
