b25715b85e80f7e5061526885edcdd73dab710f5
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2022-12-26 19:12:11 +0530

    Create LC_is_subsequence.java

---
+++ LC_is_subsequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int x=0;
+        for(int i=0;i<t.length();i++){
+            if(x==s.length()) return true;
+            if(s.charAt(x)==t.charAt(i)) x++;
+        }
+        if(x==s.length()) return true;
+        return false;
+    }
+}
