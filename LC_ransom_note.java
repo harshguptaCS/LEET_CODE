e26f0917f10a0cd703f137dd2677b78fac90f37e
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-13 22:55:17 +0530

    Create LC_ransom_note.java

---
+++ LC_ransom_note.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int []a =new int[26];
+        for(int i=0;i<magazine.length();i++){
+            a[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            a[ransomNote.charAt(i)-'a']--;
+            if(a[ransomNote.charAt(i)-'a']<0) return false;
+        }
+        return true;
+    }
+}
