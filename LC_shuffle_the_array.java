f0497dce36bc46d4df69de782b7dd2b7714aa078
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-14 22:47:01 +0530

    Create LC_shuffle_the_array.java

---
+++ LC_shuffle_the_array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int a[]=new int[nums.length];
+        for(int i=0;i<n;i++){
+            a[2*i]=nums[i];
+            a[2*i+1]=nums[n+i];
+        }
+        return a;
+    }
+}
