07f03e2720f849c7bac8b8a736cb7215a07f7703
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-01-20 01:56:59 +0530

    Update LC_power_of_two.java

--- LC_power_of_two.java
+++ LC_power_of_two.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int x=0;
-        while(n>1||n<-1){
-            if(n%2==1) return false;
-            else n=n/2;
-            x++;
-        }
-        if((n<=0&&x%2==0)||n==-1) return false;
-        return true;
+        if(n==1) return true;
+        if(n==0) return false;
+        if(n%2==1) return false;
+        else return isPowerOfTwo(n/2);
     }
 }
