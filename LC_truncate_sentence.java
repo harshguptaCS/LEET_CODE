e82316bfaaf96b26caa8c514169020a856fca0d0
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-02-16 23:42:15 +0530

    Create LC_truncate_sentence.java

---
+++ LC_truncate_sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int i;
+        for(i=0;i<s.length();i++){
+            if(s.charAt(i)==' ') k--;
+            if(k==0) break;
+        }
+        return s.substring(0,i);
+    }
+}
