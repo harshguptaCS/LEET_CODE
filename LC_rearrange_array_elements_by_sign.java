cc8723ffdfe4816888272d00cbf866afc0c97f57
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-03-25 22:53:14 +0530

    Update LC_rearrange_array_elements_by_sign.java

--- LC_rearrange_array_elements_by_sign.java
+++ LC_rearrange_array_elements_by_sign.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int [] a=new int[nums.length];
-        int l=0,r=0;
-        for(int i=0;i<nums.length/2;i++){
-            for(int j=l;j<nums.length;j++)
-            if(nums[j]>0){
-                a[2*i]=nums[j];
-                l=j+1;
-                break;
+        int l=0,r=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                a[l]=nums[i];
+                l+=2;
             }
-            for(int j=r;j<nums.length;j++)
-            if(nums[j]<0){
-                a[2*i+1]=nums[j];
-                r=j+1;
-                break;
+            else{
+                a[r]=nums[i];
+                r+=2;
             }
         }
         return a;
