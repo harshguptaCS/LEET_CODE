0f5f0ea210c6f7c1710e390b526c2a46840271ba
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-04-17 23:41:44 +0530

    Update LC_kids_with_the_greatest_number_of_candies.java

--- LC_kids_with_the_greatest_number_of_candies.java
+++ LC_kids_with_the_greatest_number_of_candies.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int m=candies[0];
-        for(int i=1;i<candies.length;i++)
-        if(candies[i]>m) m=candies[i];
+        for(int i:candies)
+        m=Math.max(m,i);
         List<Boolean> l=new ArrayList<Boolean>();
-        for(int i=0;i<candies.length;i++)
-        if(candies[i]+extraCandies>=m) l.add(true);
+        for(int i:candies)
+        if(i+extraCandies>=m) l.add(true);
         else l.add(false);
         return l;
     }
