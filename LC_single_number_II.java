f9297494c3debe46ceb416c98a9ab3802abdf863
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-06-18 17:29:16 +0530

    Create LC_single_number_II.java

---
+++ LC_single_number_II.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+		if((i==0||nums[i]!=nums[i-1])&&(i==nums.length-1||nums[i]!=nums[i+1])) return nums[i];
+        return 0;
+    }
+}
