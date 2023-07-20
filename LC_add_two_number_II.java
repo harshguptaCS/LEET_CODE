1fdc4180f0a421e6a84fc515a0609ef0b2d8e6e9
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-07-20 14:44:50 +0530

    Create LC_add_two_numbers_II.java

---
+++ LC_add_two_number_II.java
@@ -0,0 +1,38 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1=reverseList(l1);
+        l2=reverseList(l2);
+        int c=0;
+        ListNode ans=new ListNode(),temp=ans;
+        while(l1!=null||l2!=null||c==1){
+            int a=0,b=0;
+            if(l1!=null){
+                a=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                b=l2.val;
+                l2=l2.next;
+            }
+            temp.val=(a+b+c)%10;
+            if(a+b+c>9)c=1;
+            else c=0;
+            if(l1==null&&l2==null&&c==0) break;
+            ListNode i=new ListNode();
+            temp.next=i;
+            temp=i;
+        }
+        ans=reverseList(ans);
+        return ans;
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode pre=null,curr=head;
+        while(head!=null){
+            curr=head;
+            head=head.next;
+            curr.next=pre;
+            pre=curr;
+        }
+        return curr;
+    }
+}
