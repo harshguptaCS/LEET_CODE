e40d471d4bc08cec8d7fa4da480632c27542d3c9
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-08-03 15:24:08 +0530

    Create LC_binary_tree_level_order_traversal.java

---
+++ LC_binary_tree_level_order_traversal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> l=new LinkedList<>();
+        if(root==null) return l;
+        List<Integer> al=new LinkedList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        int n=1;
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode f=q.remove();
+            al.add(f.val);
+            n--;
+            if(f.left!=null) q.add(f.left);
+            if(f.right!=null) q.add(f.right);
+            if(n==0){
+                l.add(al);
+                al=new LinkedList<>();
+                al.clear();
+                n=q.size();
+            }
+        }
+        return l;
+    }
+}
