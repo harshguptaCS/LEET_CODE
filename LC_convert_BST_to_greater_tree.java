277a0289173a7d7e212e9142e0e4446ba2cf2142
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-08-06 22:31:06 +0530

    Create LC_convert_BST_to_greater_tree.java

---
+++ LC_convert_BST_to_greater_tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        harsh(root);
+        return root;
+    }
+    int x=0;
+    public void harsh(TreeNode root){
+        if(root==null) return;
+        harsh(root.right);
+        root.val=root.val+x;
+        x=root.val;
+        harsh(root.left);
+    }
+}
