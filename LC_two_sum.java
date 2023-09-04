90a114dbdc4c784f241b2f7ad64b5c557079eef1
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-09-04 20:06:13 +0530

    Update LC_two_sum.java

--- LC_two_sum.java
+++ LC_two_sum.java
@@ -1,20 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[]arr=new int[2];
-        int fl=0;
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    arr[0]=i;
-                    arr[1]=j;
-                    fl=1;
-                    break;
-                }
-            }
-            if(fl==1){
-                break;
-            }
+            if(map.containsKey(target-nums[i]))
+            return new int[]{map.get(target-nums[i]),i};
+            map.put(nums[i],i);
         }
-        return arr;
+        return new int[2];
     }
 }
