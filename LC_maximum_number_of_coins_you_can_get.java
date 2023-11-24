fe6b9f016901065f4b1e615c9131c850558fa8c8
harshguptaCS <100761020+harshguptaCS@users.noreply.github.com>
2023-11-24 21:54:45 +0530

    Create LC_maximum_number_of_coins_you_can_get.java

---
+++ LC_maximum_number_of_coins_you_can_get.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int ans=0,n=piles.length-2;
+        for(int i=0;i<piles.length/3;i++){
+            ans+=piles[n];
+            n-=2;
+        }
+        return ans;
+    }
+}
